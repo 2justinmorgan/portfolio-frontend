12f36e751c9ec9913f0fbe33a94a9d345aa27e37
Justin_Morgan <2justinmorgan@gmail.com>
2021-06-15 21:59:24 -0700

    Added eslint and applied lint-related changes BRANCH: feature/F-1_setup_npm_testing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
+import React from 'react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp: React.FunctionComponent<AppProps> = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
 }
+
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
+import React from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+const Home: React.FunctionComponent = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -23,3 +24,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home;
