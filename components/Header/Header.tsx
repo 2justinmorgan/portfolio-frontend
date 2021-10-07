6fd2cd19dd55f9172537f8235f4e96538a53a5ff
Justin_Morgan <2justinmorgan@gmail.com>
2021-10-06 22:52:15 -0700

    Moved head element from Header to app page BRANCH: feature/F-3_initialize_site_navigation

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,17 +1,8 @@
 import React from "react";
-import Head from "next/head";
 
 const Header: React.FunctionComponent = () => {
   return (
     <div data-testid="header-testid">
-      <Head>
-        <title>www.JustinLeeMorgan.com</title>
-        <meta
-          name="description"
-          content="The porfolio site of Justin Lee Morgan"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
     </div>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
@@ -6,7 +7,19 @@ const MyApp: React.FunctionComponent<AppProps> = ({
   Component,
   pageProps,
 }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>www.JustinLeeMorgan.com</title>
+        <meta
+          name="description"
+          content="The porfolio site of Justin Lee Morgan"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
