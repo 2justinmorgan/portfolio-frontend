b68d9e22a30d3a420a3ee05b0ecd000e8deb910d
Justin_Morgan <2justinmorgan@gmail.com>
2021-10-12 23:55:07 -0700

    Added AppBar and NavMenu components to Header BRANCH: feature/F-3_initialize_site_navigation

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,8 +1,39 @@
 import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Logo from "../Logo/Logo";
+import NavMenu from "../NavMenu/NavMenu";
 
 const Header: React.FunctionComponent = () => {
+  const [showNavMenu, toggleShowNavMenu] = React.useState(false);
+
   return (
     <div data-testid="header-testid">
+      <AppBar position="static" style={{ backgroundColor: "#c3c3c3" }}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={() => toggleShowNavMenu(!showNavMenu)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <div style={{ paddingTop: "5px" }}>
+            <Logo />
+          </div>
+        </Toolbar>
+      </AppBar>
+      {showNavMenu && (
+        <NavMenu
+          isOpen={showNavMenu}
+          toggleOpen={toggleShowNavMenu}
+        />
+      )}
     </div>
   );
 };
