3bad6201a2dc838e3702bcf2b044355a66705aba
Justin_Morgan <2justinmorgan@gmail.com>
2021-10-18 20:48:04 -0700

    Commonized all pages in app page BRANCH: feature/F-3_initialize_site_navigation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import Header from "../components/Header/Header";
+import Container from "@mui/material/Container";
+import Card from "@mui/material/Card";
 import Footer from "../components/Footer/Footer";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
@@ -9,8 +12,13 @@ const MyApp: React.FunctionComponent<AppProps> = ({
   Component,
   pageProps,
 }: AppProps) => {
+  const router = useRouter();
+
   return (
-    <div data-testid="myapp-testid">
+    <div
+      data-testid="myapp-testid"
+      style={{ height: "100vh", backgroundColor: "#ecebec" }}
+    >
       <Head>
         <title>www.JustinLeeMorgan.com</title>
         <meta
@@ -20,7 +28,20 @@ const MyApp: React.FunctionComponent<AppProps> = ({
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <Component {...pageProps} />
+      <br />
+      <br />
+      <Container maxWidth="lg">
+        <Card
+          raised={true}
+          sx={{ padding: "0 20px 20px 20px", backgroundColor: "#f3f3f3" }}
+        >
+          <h1>
+            {router.pathname.charAt(1).toUpperCase() + router.pathname.slice(2)}
+          </h1>
+          <hr style={{ margin: "-10px 0 30px 0" }} />
+          <Component {...pageProps} />
+        </Card>
+      </Container>
       <Footer />
     </div>
   );
