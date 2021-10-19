b354f2adb1e558ec42fada048ee5afee16c69dae
Justin_Morgan <2justinmorgan@gmail.com>
2021-10-18 21:55:28 -0700

    Added close chevron button to NavMenu BRANCH: feature/F-3_initialize_site_navigation

--- components/NavMenu/NavMenu.tsx
+++ components/NavMenu/NavMenu.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import Divider from "@mui/material/Divider";
 import Box from "@mui/material/Box";
 import Link from "next/link";
 import List from "@mui/material/List";
@@ -9,6 +12,7 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
+import styles from "./NavMenu.module.css";
 
 const propTypes = {
   isOpen: PropTypes.bool.isRequired,
@@ -23,7 +27,16 @@ const NavMenu: React.FunctionComponent<NavMenuPropsT> = (props) => {
 
   return (
     <Drawer open={isOpen} onClose={() => toggleOpen(false)}>
-      <Box sx={{ width: "200px" }} role="presentation">
+      <h3 className={styles.title}>
+        Site Navigation
+        <span className={styles.closebtn}>
+          <IconButton onClick={() => toggleOpen(false)}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </span>
+      </h3>
+      <Divider />
+      <Box sx={{ width: "210px" }} role="presentation">
         <List>
           {views.map((text, index) => (
             <Link href={"/" + text.toLowerCase()} key={index}>
